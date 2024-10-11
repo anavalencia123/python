cc8b1fe10e25fee0818b40d214efc0fb67d1c6c6
anavalencia123 <martinezlauramartinez2006@gmail.com>
2024-10-11 15:30:44 -0500

    ..

--- proyecto 2/main.py
+++
@@ -1,7 +0,0 @@
-peliculas = [
-    {"nombre": "el cadaver de la novia","descripcion":,"En un pequeño, triste y reprimido pueblo victoriano, dos tímidos jóvenes están a punto de casarse - aunque no se han visto nunca antes. Los burdos y arribistas magnates de las conservas de pescado Nell y William Van Dort (con las voces de Tracey Ullman y Paul Whitehouse) siempre han soñado con formar parte de la alta sociedad - pero si bien no les falta riqueza, carecen por completo de la clase necesaria","genero":["Fantacia"],"director":["tim burton"]
-    },
-]
-def mostar_todas_las_peliculas(peliculas):
-    for pelcula in peliculas:
-        print(f(n))
